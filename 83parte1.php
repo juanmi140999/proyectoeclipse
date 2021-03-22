62da09e554d40b61aa2069a15bb4f0d7c4e76cb2
juanmi140999 <juanmiguel.saugar@campusfp.es>
2021-03-22 10:54:06 +0100

    main83parte1

---
+++ 83parte1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primer grado</title>
+</head>
+<body>
+    <form action="83parte1.php" method="post">
+		numerador: <input type="decimal" name="numerador" size="10">
+		denominador: <input type="decimal" name="denominador" size="10">
+		<input type="submit" value="Ecuación 1º grado">
+	</form>
+    <?php
+        $numerador = $_POST["numerador"];
+        $denominador = $_POST["denominador"];
+        $divisor = 2;
+        
+        while ($divisor<=$numerador && $divisor<=$denominador){
+            while(($numerador%$divisor)==0 && ($numerador%$divisor)==0){
+                $numerador = $numerador / $divisor;
+                $denominador = $denominador / $divisor;
+            }
+            $divisor++;
+        }
+        echo "Fraccion simplificada: ".$numerador."\\".$denominador;
+    ?>
+</body>
+</html>
